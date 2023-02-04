31e624cd493d3908449e4c6ac6446bcdabfdc0f9
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2023-02-04 15:15:19 +0530

    added face recognition from live videos

---
+++ testing_files/7_recognize_video.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+import os
+
+haar_cascade = cv.CascadeClassifier('testing_files\haar_face.xml')
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+face_recognizer.read(r'testing_files\face_trained.yml')
+
+DIR = r'C:\Users\DAVASR\Documents\AFRAAS_practice\train\persons'
+peoples = []
+
+for i in os.listdir(DIR):
+    peoples.append(i)
+
+capture = cv.VideoCapture(1)
+
+while(True):
+    isTrue, frame = capture.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7)
+
+    for(x, y, w, h) in faces_rect:
+
+        faces_roi = gray[y:y+h, x:x+w]
+
+        label, confidence = face_recognizer.predict(faces_roi)
+        if confidence > 40 and confidence < 100:
+            
+
+            print(f"label : {peoples[label]}, with a confidence of {confidence}")
+
+            cv.putText(frame, str(peoples[label]), (x, y+h+30), cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), thickness=2)
+
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+        
+    
+    cv.imshow("video", frame)
+
+    if cv.waitKey(1) == ord('e'):
+        break
