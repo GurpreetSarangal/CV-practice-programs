60a4f42e5ab41f09e394784b9e48fc4ad3df2623
GurpreetSarangal <gurpreetsarangal7@gmail.com>
2023-02-11 14:46:51 +0530

    changes on 11/2

--- AFRAAS/Camera.py
+++ AFRAAS/Camera.py
@@ -8,20 +8,37 @@ class Camera:
         self.modes = [(0, cv.CAP_DSHOW), (1, None)]
         self.pathToCascade = r"resources\models\haar_face.xml"
         self.pathToRecognizer = r"resources\models\face_trained.yml"
+        self.pathToDatabase = r"resources\database\persons"
 
     def connect(self, mode):
-        pass
+        if mode[1] == None:
+            capture = cv.VideoCapture(mode[0])
+        else:
+            capture = cv.VideoCapture(mode[0], mode[1])
+        
+        return capture
 
-    def getFrame(self):
-        pass
+    def getFrame(self, capture):
+        isRead, Frame = capture.read()
+        if isRead:
+            return Frame
+        else:
+            return None
+        
+    def cropToFace(self, Frame):
+        haar_cascade = cv.CascadeClassifier(self.pathToCascade)
+        gray = self.toGrayScale(Frame)
+        frame_roi = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7)
 
-    def cropToFace(self):
-        pass
+        first_face_indices = frame_roi[0]
+        x, y, w, h = first_face_indices
+        return Frame[x:x+w, y:y+h]
 
-    def toGrayScale(self):
-        pass
-    def saveFrame(self):
-        pass
+    def toGrayScale(self, Frame):
+        return cv.cvtColor(Frame, cv.COLOR_BGR2GRAY)
+    
+    def saveFrame(self, Frame, path=r"resources\database\persons"):
+        
     def addNewFace(self):
         pass
     def updateFace(self):
