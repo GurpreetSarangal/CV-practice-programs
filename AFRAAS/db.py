64a4ee3f9ad9af18c9875e0eb50405ea2ee5bc7e
GurpreetSarangal <gurpreetsarangal7@gmail.com>
2023-03-18 12:43:59 +0530

    connection with remote database

--- AFRAAS/db.py
+++ AFRAAS/db.py
@@ -9,16 +9,33 @@ class db:
         dotenv_path = Path(r'AFRAAS\resources\user.env')
         load_dotenv(dotenv_path = dotenv_path)
         self.conn = mysql.connector.connect(
-            host=os.getenv('AFRAAS_HOST'),
-            user=os.getenv('AFRAAS_USER'),
-            password=os.getenv('AFRAAS_PASSWORD'),
-            database=os.getenv('AFRAAS_DATABASE'),
+            host="sql12.freemysqlhosting.net",
+            user="sql12606276",
+            password="phgYMTLRzM",
+            database="sql12606276",
         )
+        # self.conn = mysql.connector.connect(
+        #     host=os.getenv('AFRAAS_HOST'),
+        #     user=os.getenv('AFRAAS_USER'),
+        #     password=os.getenv('AFRAAS_PASSWORD'),
+        #     database=os.getenv('AFRAAS_DATABASE'),
+        # )
 
     def check(self):
         cursor = self.conn.cursor()
-        cursor.execute("select table_name, table_type, table_schema from information_schema.TABLES where table_schema like 'afraas';")
+        cursor.execute("select table_name, table_type, table_schema from information_schema.TABLES where table_schema like 'sql12606276';")
+        check = ('attendance', 'departments', 'shifts', 'users')
+        db_tables = []
+        for table in cursor:
+            db_tables.append(str(table[0]).lower())
         
+        # print(db_tables)
+        for table in check:
+            if table not in db_tables:
+                print(table," is not there")
+                return False
+        
+        return True
 
     def addNewUser(self,name, dept_id, shift_id):
         cursor = self.conn.cursor()
@@ -32,6 +49,7 @@ class db:
         pass
 
     def addNewDept(self, dept_name):
+        
         pass
     def updateDeptName(self, dept_id, new_name):
         pass
@@ -56,3 +74,44 @@ class db:
 
     def executeCustomQuery(self, query):
         pass
+    
+    def createTables(self):
+        cursor = self.conn.cursor()
+        cursor.execute("show databases")
+        for x in cursor:
+            print(x)
+
+        cursor.execute('''
+        CREATE TABLE Departments(
+            dept_id int,
+            dept_name VARCHAR(255),
+            PRIMARY KEY(dept_id)
+        ); ''')
+
+        cursor.execute('''
+        CREATE TABLE Shifts(
+            shift_id int,
+            time_in TIME, 
+            PRIMARY KEY(shift_id)
+        ); ''')
+
+        cursor.execute('''
+        CREATE TABLE Users(
+            name VARCHAR(255) NOT NULL,
+            dept_id int, 
+            shift_id int NOT NULL,
+            user_id int,
+            PRIMARY KEY (user_id),
+            FOREIGN KEY (dept_id) REFERENCES Departments (dept_id),
+            FOREIGN KEY (shift_id) REFERENCES Shifts (shift_id)
+        ); ''')
+
+        cursor.execute('''
+        CREATE TABLE Attendance(
+            rec_id int,
+            time_stamp TIME,
+            status enum("enter", "exit", "absent"),
+            user_id int,
+            PRIMARY KEY (rec_id),
+            FOREIGN KEY (user_id) REFERENCES Users(user_id)
+        ); ''')
